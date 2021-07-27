55fe007dbd0431702544c26692a8fd5ce3f088c6
Ivan MPR <ivsanmas@gmail.com>
2021-07-27 16:12:19 +0200

    final code

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 'use strict';
+// Selections
 // /////////////////////////////////////////////////////////////////
 const outputParagraph = document.querySelector('.password-output');
 const copyContentBtn = document.querySelector('.btn-copy');
 const lengthInput = document.querySelector('.input-length');
-const generate = document.querySelector('.generate');
+const generateBtn = document.querySelector('.generate');
 // //////////////////////////////////////////////////////////////////
 const numbersChkbox = document.querySelector('.numbers');
 const symbolsChkbox = document.querySelector('.symbols');
 const smallLettersChkbox = document.querySelector('.small-letters');
 const bigLettersChkbox = document.querySelector('.big-letters');
 const checkBoxes = document.querySelectorAll('input[type=checkbox]');
-// console.log(checkBoxes);
 // /////////////////////////////////////////////////////////////////
 // Numbers
 function randomNumber() {
   const numbers = Array.from({ length: 10 }, (_, i) => String(i));
   return shuffle(numbers)[0];
 }
-// Uppercase Alphabet
+// Uppercase Letter
 function randomUpLetter() {
   const upperAlpha = Array.from(Array(26)).map((_, i) => i + 65);
   const upperCaseAlphabet = upperAlpha.map(el => String.fromCharCode(el));
   return shuffle(upperCaseAlphabet)[0];
 }
-// Lowercase Alphabet
+// Lowercase Letter
 function randomLoLetter() {
   const lowerAlpha = Array.from(Array(26)).map((_, i) => i + 97);
   const lowerCaseAlphabet = lowerAlpha.map(el => String.fromCharCode(el));
   return shuffle(lowerCaseAlphabet)[0];
 }
-// Special Characters
+// Special Characters/Symbols
 function randomSymbol() {
-  // const string = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
-  const string = '!#$%&*+-/<>?@^_`';
+  const string = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
+  // const string = '!#$%&*+-/<>?@^_`';
   const specChars = string.split('');
   return shuffle(specChars)[0];
 }
+// Fisher–Yates shuffle
+// /////////////////////////////////////////////////////
 function shuffle(array) {
   var currentIndex = array.length,
     temporaryValue,
@@ -52,77 +54,82 @@ function shuffle(array) {
   }
   return array;
 }
-
-generate.addEventListener('click', function () {
+// /////////////////////////////////////////////////////
+generateBtn.addEventListener('click', function () {
   // //////////////////////////////////////
   isInputValid(lengthInput);
   if (areAllBoxesUnchecked(checkBoxes)) {
     alert('Check at least one box !');
+    outputParagraph.value = '';
     return;
   }
   // /////////////////////////////////////
-  function limit() {
-    const limit = +lengthInput.value || 7;
-    return limit === 7 ? 7 : limit - 1;
-  }
-  // ////////////////////////////////////////////
-
   filterChecked(checkBoxes);
-
+  // ////////////////////////////////////////////
+  const limit = +lengthInput.value || 8;
+  // ////////////////////////////////////////////
   const checkedArrLength = filterChecked(checkBoxes).length;
-  const calcLength = Math.ceil(limit() / checkedArrLength);
-  console.log(limit() / checkedArrLength);
-  console.log(limit());
-  console.log(calcLength);
-  console.log(checkedArrLength);
+  const calcLength = Math.ceil(limit / checkedArrLength);
+  // ////////////////////////////////////////////
   const test = Array(calcLength).fill(filterChecked(checkBoxes)).flat();
-  console.log(test);
   // ///////////////////////////////////////////
-  // const output = [];
+  trimTestArrayIfLongerThanLimit(test, limit);
   // ///////////////////////////////////////////
-  function fillOutputArray(arr) {
-    const choice = function (el) {
-      return el === 'rN'
-        ? randomNumber()
-        : el === 'rS'
-        ? randomSymbol()
-        : el === 'rLL'
-        ? randomLoLetter()
-        : randomUpLetter();
-    };
-
-    return test.map(el => choice(el));
+  function fillOutputArray() {
+    return test.map(el => fnChoice(el));
   }
   // ///////////////////////////////////////////
-  outputParagraph.value = shuffle(
-    fillOutputArray([...filterChecked(checkBoxes)])
-  ).join('');
-  // //////////////////////////////////////////////
+  outputParagraph.value = shuffle(fillOutputArray()).join('');
 });
-
-function copyPassword() {
-  if (outputParagraph.value !== '') {
-    outputParagraph.select();
-    document.execCommand('copy');
-    console.log('copied');
-    alert('Copied the text: ' + outputParagraph.value);
+//////////////////////////////////////////////////////////////
+function fnChoice(element) {
+  return element === 'rN'
+    ? randomNumber()
+    : element === 'rS'
+    ? randomSymbol()
+    : element === 'rLL'
+    ? randomLoLetter()
+    : randomUpLetter();
+}
+//////////////////////////////////////////////////////////////
+function trimTestArrayIfLongerThanLimit(arr, limitValue) {
+  if (arr <= limitValue) {
+    return arr;
   } else {
-    alert('Empty');
+    let length = arr.length;
+    while (length > limitValue) {
+      arr.pop();
+      length--;
+    }
+    return arr;
   }
 }
-copyContentBtn.addEventListener('click', copyPassword);
-
 // ///////////////////////////////////////////////////////////
 function areAllBoxesUnchecked(list) {
   return Array.from(list).every(input => !input.checked);
 }
+// ///////////////////////////////////////////////////////////
 function isInputValid(input) {
   if (!isFinite(input.value)) {
     alert('Enter numeric value !');
     input.value = '';
   }
 }
+// ///////////////////////////////////////////////////////////
 function filterChecked(list) {
   const test = Array.from(list).filter(input => input.checked);
   return test.map(el => el.dataset.description);
 }
+//////////////////////////////////////////////////////////////
+// Copy-Paste functionality
+// //////////////////////////////////////////////////////////
+function copyPassword() {
+  if (outputParagraph.value !== '') {
+    outputParagraph.select();
+    document.execCommand('copy');
+    alert('Copied the text: ' + outputParagraph.value);
+  } else {
+    alert('Empty');
+  }
+}
+copyContentBtn.addEventListener('click', copyPassword);
